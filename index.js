57618f700ebfda8e9b03e694351e1036187a189e
lilian manaa <lelian.ibraheem@gmail.com>
2022-09-23 10:30:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+saturdayFun();
+saturdayFun("skatinig")
+
+const mondayWork =function(activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+mondayWork();
+mondayWork("meeting")
+
+function wrapAdjective(symbol="*"){
+    return function(string){
+        return`You are ${symbol}${string}${symbol}!`
+    }
+}
+
+console.log(wrapAdjective("%")("a dedicated programmer"));
+console.log(wrapAdjective("||")("a dedicated programmer"));
